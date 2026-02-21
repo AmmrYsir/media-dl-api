97393559f7b50882257852e13bfb9585f8b6abdb
AmmrYsir <ammaryasir03062000@gmail.com>
2026-02-21 08:59:00 +0800

    add comment

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000"], # This is related to the frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
